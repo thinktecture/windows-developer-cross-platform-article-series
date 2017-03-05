dbd30684bb4bc958b2c3c4e4d0d0fc29af772746
Christian Weyer <christian.weyer@thinktecture.com>
2017-03-05 21:01:29 +0100

    Simple wrapper for app content with BS4

--- part1/src/app/components/contentHost/content-host.ts
+++ part1/src/app/components/contentHost/content-host.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-content-host',
   templateUrl: 'content-host.html',
-  styleUrls: ['content-host.scss']
+  styleUrls: ['content-host.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContentHostComponent {
 }
--- part1/src/app/components/list/list.ts
+++ part1/src/app/components/list/list.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {StarWarsService} from '../../services/starWars';
 import {ActivatedRoute, Router} from '@angular/router';
 import {StarWarsBase} from '../../models/starWarsBase';
