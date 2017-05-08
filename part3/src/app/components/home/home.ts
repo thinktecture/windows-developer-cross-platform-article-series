e70fb0b6424f54e2fba638bc6f9a2e62360779b2
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-05-08 12:40:53 +0200

    UI Tweaks

--- part3/src/app/components/home/home.ts
+++ part3/src/app/components/home/home.ts
@@ -2,8 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.html',
-  styleUrls: ['home.scss']
+  templateUrl: 'home.html'
 })
 export class HomeComponent {
 
