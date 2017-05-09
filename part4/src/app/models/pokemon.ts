b331c8ec75702fbf5d5996fc766e76b2db9de51e
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-05-09 08:17:13 +0200

    Electron Integration

---
+++ part4/src/app/models/pokemon.ts
@@ -0,0 +1,24 @@
+import {BaseModel} from './baseModel';
+import {ModelHelperService} from '../services/modelHelper';
+
+export class Pokemon implements BaseModel {
+  public id: number;
+  public name: string;
+  public weight: number;
+  public baseExperience: string;
+  public height: string;
+
+  public static deserialize(obj: any): Pokemon {
+    const pokemon = new Pokemon();
+
+    // Copy over all values
+    pokemon.name = obj.name;
+    pokemon.weight = obj.weight;
+    pokemon.baseExperience = obj.base_experience;
+    pokemon.height = obj.height;
+
+    pokemon.id = obj.id ? obj.id : ModelHelperService.extractIdFromUrl(obj.url);
+
+    return pokemon;
+  }
+}
